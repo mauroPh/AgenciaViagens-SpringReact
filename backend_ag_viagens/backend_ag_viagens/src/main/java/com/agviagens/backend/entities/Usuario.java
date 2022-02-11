612dcd2b10f04fe865ce4b763d15bee70f7a0ddd
mauro <developer@debian-BULLSEYE-live-builder-AMD64>
2022-02-11 17:09:22 -0300

    H2 database,perfil de teste e JPA

--- backend_ag_viagens/backend_ag_viagens/src/main/java/com/agviagens/backend/entities/Usuario.java
+++ backend_ag_viagens/backend_ag_viagens/src/main/java/com/agviagens/backend/entities/Usuario.java
@@ -1,11 +1,20 @@
 package com.agviagens.backend.entities;
-
 import java.io.Serializable;
+
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
+	
+		@Id
+		@GeneratedValue(strategy =GenerationType.IDENTITY)
 		private Long id;
 		private String nome;
 		private String email;
