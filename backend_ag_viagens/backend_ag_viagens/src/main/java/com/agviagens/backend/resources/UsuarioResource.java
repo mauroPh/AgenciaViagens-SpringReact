ddccd004675624467798a1ab2b21ac4fcb8fd638
mauroPh <mauro.philipe.santos@gmail.com>
2022-02-14 22:08:08 -0300

    dev profile

--- backend_ag_viagens/backend_ag_viagens/src/main/java/com/agviagens/backend/resources/UsuarioResource.java
+++ backend_ag_viagens/backend_ag_viagens/src/main/java/com/agviagens/backend/resources/UsuarioResource.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,5 +44,19 @@ public class UsuarioResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj); 
 	}
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Usuario> update(@PathVariable Long id, @RequestBody Usuario obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
 
+	
+	
+	
 }
--- backend_ag_viagens/backend_ag_viagens/src/main/java/com/agviagens/backend/services/UsuarioService.java
+++ backend_ag_viagens/backend_ag_viagens/src/main/java/com/agviagens/backend/services/UsuarioService.java
@@ -3,11 +3,17 @@ package com.agviagens.backend.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.agviagens.backend.entities.Usuario;
 import com.agviagens.backend.repositories.UsuarioRepository;
+import com.agviagens.backend.services.exceptions.DatabaseException;
+import com.agviagens.backend.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -26,8 +32,33 @@ public class UsuarioService {
 	}
 	
 	public Usuario insert(Usuario obj) {
-		return	repository.save(obj);
+		return repository.save(obj);
 	}
 	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Usuario update(Long id, Usuario obj) {
+		try {
+			Usuario entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}	
+	}
+
+	private void updateData(Usuario entity, Usuario obj) {
+		entity.setNome(obj.getNome());
+		entity.setEmail(obj.getEmail());
+		entity.setTelefone(obj.getTelefone());
+	}
 	
 }
