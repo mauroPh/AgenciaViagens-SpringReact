9a5f8afc6802d8b4effa026f3cd50e35e71ce546
mauroPh <mauro.philipe.santos@gmail.com>
2022-02-14 23:22:13 -0300

    Ajustes no frontend

--- frontend_ag_viagens/src/App.tsx
+++ frontend_ag_viagens/src/App.tsx
@@ -5,11 +5,9 @@ import {
 } from "react-router-dom";
 import Listing from 'pages/Listing';
 import Form from 'pages/Form';
-import Home from 'pages/Home';
 import Navbar from "components/Navbar";
 import Login from "pages/Login";
 import Cadastro from "pages/Login/cadastro";
-import Footer from "components/Footer";
 
 function App() {
   return (
@@ -17,7 +15,7 @@ function App() {
 
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Listing />} />
         <Route path="/destinos" element={<Listing />} />
         <Route path="/form">
           <Route path=":pacoteId" element={<Form />} />
